8a8989b278a4a30b6436388d6af4a50c032a66db
charpa <patrick.chardavoine@gmail.com>
2022-11-09 14:41:34 +0100

    ch11

--- django_project/urls.py
+++ django_project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     # Local apps
     ## path("accounts/", include("accounts.urls")),
-    path("", include("pages.urls")),  #
+    path("", include("pages.urls")),
+    path("books/", include("books.urls")),
 ]
